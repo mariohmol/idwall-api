1d678154d0f4a760184bae08333ce024da3775b9
Mario Mol <mariohmol@gmail.com>
2019-04-28 14:30:20 -0300

    working example

--- api.js
+++ api.js
@@ -1,50 +1,49 @@
 const superagent = require('superagent');
+const token = 'seutoken';
+
+
 
 /**
  * 
- * @param {*} data 
- * {
-//       	"matriz": "consultaPessoaDefault",
-//         "parametros": {
-//           "cpf_data_de_nascimento": "20/08/1881",
-//           "cpf_nome": "Alvo Percival Wulfric Brian Dumbledore",
-//           "cpf_numero": "71393401881"
-//       	}
-//       }
+ * ID WALL 
+ * 
+ * 
  */
-const token = 'seutoken';
-
-const criarRelatorio = async function(data){
-    const reqWS = await superagent.post({
-        url: 'https://api-v2.idwall.co/relatorios',
-        headers : {
-            'Content-Type': 'application/json',
-            'Authorization': token
-        },
-        data
-    });
+/**
+* 
+* @param {*} data 
+* {
+*       	"matriz": "consultaPessoaDefault",
+*         "parametros": {
+*           "cpf_data_de_nascimento": "20/08/1881",
+*           "cpf_nome": "Alvo Percival Wulfric Brian Dumbledore",
+*           "cpf_numero": "71393401881"
+*       	}
+*       }
+*/
+const criarRelatorio = async function (data) {
+    const reqWS = await superagent
+        .post('https://api-v2.idwall.co/relatorios')
+        .send(data)
+        .set('Authorization', token)
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
     return reqWS;
 }
 
 // Obtendo status de um relatório
-const statusRelatorio = async function(numero_relatorio){
-    const reqWS = await superagent.get({
-        url: `https://api-v2.idwall.co/relatorios/${numero_relatorio}`,
-        headers : {
-            'Authorization': token
-        }
-    });
+const statusRelatorio = async function (numero_relatorio) {
+    const reqWS = await superagent.get(`https://api-v2.idwall.co/relatorios/${numero_relatorio}`)
+        .set('Authorization', token);
     return reqWS;
 }
 
-const dadosRelatorio = async function(numero_relatorio){
-    const reqWS = await superagent.get({
-        url: `https://api-v2.idwall.co/relatorios/${numero_relatorio}/dados`,
-        headers : {
-            'Authorization': token
-        }
-    });
+const dadosRelatorio = async function (numero_relatorio) {
+    const reqWS = await superagent.get(`https://api-v2.idwall.co/relatorios/${numero_relatorio}/dados`)
+        .set('Authorization', token);
     return reqWS;
 }
 
-module.exports = { criarRelatorio, statusRelatorio, dadosRelatorio};
+export { router, getNewReportRequests, idWallScheduler };
+
+module.exports = { criarRelatorio, statusRelatorio, dadosRelatorio };
